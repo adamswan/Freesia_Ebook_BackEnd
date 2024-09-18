62927ceb815b5ecadd5d00c6cf85dec17dbf6590
Adams Wan <melyling@sina.com>
2024-09-18 22:12:47 +0800

    feat: 后端完成电子书的上传接口，带有文件类型校验的接口

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,17 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, UseInterceptors, UploadedFile, ParseFilePipeBuilder } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { BookSearch } from 'src/types/book';
 import { FormattInterceptor } from 'src/formatt-interceptor/formatt.interceptor';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs'
+import * as path from 'path'
 
 @Controller('book')
 export class BookController {
-  constructor(private readonly bookService: BookService) {}
+  constructor(private readonly bookService: BookService) { }
 
-  @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.bookService.create(createBookDto);
+  // 上传电子书
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadBook(@UploadedFile(new ParseFilePipeBuilder().addFileTypeValidator({ fileType: 'epub' }).build()) file: Express.Multer.File) {
+
+    console.log('file', file);
+
+    const originalname = file.originalname // 文件原始名称
+    const mimetype = file.mimetype // 文件媒体格式
+    const size = file.size // 文件大小
+
+    // Nginx 的静态资源目录
+    const nginxStaticPath = 'E:/Nginx/html/uploadFile'
+    // 绝对路径
+    const absPath = path.resolve(nginxStaticPath, originalname)
+    // 同步写入文件
+    fs.writeFileSync(absPath, file.buffer)
+
+    return {
+      code: 0,
+      result: {
+        originalname,
+        mimetype,
+        size,
+        path: absPath
+      },
+      message: '上传成功'
+    }
   }
 
   // 获取所有书
